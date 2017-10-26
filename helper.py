ec0e140d054a5bedb64d487e581875b05a4cdf8f
Andrew Moore <a.moore@lancaster.ac.uk>
2017-10-26 13:59:40 +0100

    Read config file and get package directory

--- helper.py
+++ helper.py
@@ -1,1 +1,44 @@
+'''
+Functions that are used throughout the package:
 
+Functions:
+1. :py:func:`tdparse.helper.read_config`
+'''
+import os
+
+from ruamel.yaml import YAML
+
+import tdparse
+
+def package_dir():
+    '''
+    :returns: The path to the directory of the package
+    :rtype: String
+    '''
+
+    return tdparse.__path__._path[0]
+
+
+
+def read_config(config_key, config_file_name='config.yaml'):
+    '''
+    :param config_key: key to a value stored in the config.yaml file
+    :param config_file_name: Name of the config file within the package directory
+    :type config_key: String
+    :type config_file_name: String Default `config.yaml`
+    :returns: Value stored at the keys location
+    :rtype: Python type of the value e.g. if 5 it will be int
+    '''
+
+    config_file_path = os.path.join(package_dir(), config_file_name)
+    if not os.path.isfile(config_file_path):
+        raise FileNotFoundError('The following config file does not exist {}'\
+                                .format(config_file_path))
+    with open(config_file_path, 'r') as config_file:
+        yaml = YAML()
+        config_data = yaml.load(config_file)
+        if config_key in config_data:
+            return config_data[config_key]
+        else:
+            raise ValueError('This key {} does not exist in the config file {}'\
+                             .format(config_key, config_file_path))
--- tests/test_helper.py
+++ tests/test_helper.py
@@ -1,11 +1,40 @@
-import unittest
+'''
+Unit tests for the :py:mod:`tdparse.helper` module.
+'''
+import os
+
+from unittest import TestCase
+
 
 from tdparse.helper import read_config
+from tdparse.helper import package_dir
 
-class TestHelper(unittest.TestCase):
+class TestHelper(TestCase):
+    '''
+    Contains the following functions:
+    :py:func:`tdparse.helper.read_config`
+    '''
     def test_read_config(self):
+        '''
+        Tests :py:func:`tdparse.helper.read_config`
+        '''
+
         self.assertEqual(read_config('unit_test_dong_data'),
                          './tests/data/dong_sent.txt')
-        with self.assertRaises(ValueError):
-            read_config('nothing here',
-                        msg='nothing here should not be in the config.yaml')
+        with self.assertRaises(ValueError,
+                               msg='nothing here should not be in the config.yaml'):
+            read_config('nothing here')
+        test_config_name = 'doesnotexist'
+        with self.assertRaises(FileNotFoundError,
+                               msg='there should be no file named {}'\
+                                   .format(test_config_name)):
+            read_config('unit_test_dong_data', config_file_name=test_config_name)
+
+    def test_package_dir(self):
+        '''
+        Tests :py:func:`tdparse.helper.package_dir`
+        '''
+
+        self.assertIsInstance(package_dir(), str, msg='The return should be a String')
+        self.assertEqual(package_dir().split(os.sep)[-1], 'tdparse',
+                         msg='The last folder should be tdparse')
