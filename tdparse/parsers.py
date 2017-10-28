e1a8c4927e59b22734f0fbe3665bd2b013aa74fb
Andrew Moore <andrew.p.moore94@gmail.com>
2017-10-28 16:06:15 +0100

    Created a parser for the dong sentiment data

--- tdparse/parsers.py
+++ tdparse/parsers.py
@@ -4,6 +4,7 @@ annotated dataset are the following:
 1. `Li Dong <http://goo.gl/5Enpu7>`_ which links to :py:func:`tdparse.parsers.dong`
 '''
 import os
+import re
 
 def dong(file_path):
     '''
@@ -17,8 +18,41 @@ def dong(file_path):
     if not os.path.isfile(file_path):
         raise FileNotFoundError('This file does not exist {}'.format(file_path))
 
+    sentiment_range = [-1, 0, 1]
+
     sentiment_data = []
     with open(file_path, 'r') as dong_file:
+        sent_dict = {}
         for index, line in enumerate(dong_file):
+            divisible = index + 1
             line = line.strip()
+            if divisible % 3 == 1:
+                sent_dict['text'] = line
+            elif divisible % 3 == 2:
+                sent_dict['target'] = line
+            elif divisible % 3 == 0:
+                sentiment = int(line)
+                if sentiment not in sentiment_range:
+                    raise ValueError('The sentiment has to be one of the following '\
+                                     'values {} not {}'.format(sentiment_range, sentiment))
+                sent_dict['sentiment'] = int(line)
+                text = sent_dict['text']
+                target = sent_dict['target']
+                # Getting offsets
+                offsets = []
+                while True:
+                    match = re.search(r'\$T\$', text)
+                    if match is None:
+                        break
+                    start_offset = match.span()[0]
+                    end_offset = start_offset + len(sent_dict['target'])
+                    offsets.append([start_offset, end_offset])
+                    text = re.sub(r'\$T\$', target, text, count=1)
+                sent_dict['text'] = text
+                sent_dict['id'] = len(sentiment_data)
+                sent_dict['spans'] = offsets
+                sentiment_data.append(sent_dict)
+                sent_dict = {}
+            else:
+                raise Exception('Problem')
     return sentiment_data
--- tests/test_parsers.py
+++ tests/test_parsers.py
@@ -11,7 +11,6 @@ class TestParsers(TestCase):
     '''
     Contains the following functions:
     :py:func:`tdparse.parsers.test_dong`
-    :py:func:`tdparse.helper.test_package_dir`
     '''
 
     def test_dong(self):
@@ -19,43 +18,80 @@ class TestParsers(TestCase):
         Tests :py:func:`tdparse.parsers.dong`
         '''
 
+        def check_results(expected_results, test_results):
+            '''
+            Given the expected results and the results from the function beign
+            tested it will test that they are both equal. It will return nothing
+            but will test if they are correct else it fails the tests.
+
+            :param expected_results: A list of dictionaries containing expected
+            values
+            :param test_results: A list of dictionaries containing results from
+            the function that is being tested
+            :type expected_results: list
+            :type test_results: list
+            :returns: Nothing but checks if the results are to be expected
+            :rtype: None
+            '''
+
+            for index, expected_result in enumerate(expected_results):
+                test_result = test_results[index]
+                for key, expected_value in expected_result.items():
+                    test_value = test_result[key]
+                    self.assertIsInstance(expected_value, type(test_value),
+                                          msg='The expected value : {} is not of the '\
+                                          'same type as the tested value : {}'\
+                                          .format(type(expected_value), type(test_value)))
+                    if key == 'spans':
+                        test_value = sorted(test_value, key=lambda x: x[0])
+                        expected_value = sorted(expected_value, key=lambda x: x[0])
+
+                    self.assertEqual(expected_value, test_value,
+                                     msg='Expected {} returned {}'.format(expected_value,
+                                                                          test_value))
+
+
         test_file_path = 'anything'
         with self.assertRaises(FileNotFoundError, msg='there should be no file named {}'\
                                .format(test_file_path)):
             dong(test_file_path)
 
         test_file_path = './tests/test_data/dong_test_data.txt'
-        expected_results = [{'id':'dong_train_0',
+        expected_results = [{'id':0,
                              'sentiment':-1,
                              'text':'This is a fake news article that is to represent a Tweet!!!!',
                              'target':'news article',
-                             'span':(15, 26)},
-                            {'id':'dong_train_1',
+                             'spans':[[15, 27]]},
+                            {'id':1,
                              'sentiment':1,
                              'text':'I had a great day however I did not get much work done',
                              'target':'day',
-                             'span':(14, 17)},
-                            {'id':'dong_train_2',
+                             'spans':[[14, 17]]},
+                            {'id':2,
                              'sentiment':0,
                              'text':'I cycled in today and it was ok as it was not raining.',
                              'target':'cycled',
-                             'span':(2, 7)}]
-        # Ensures it extracts the correct dictionary structure
-        test_results = dong(test_file_path)
-        for index, test_result in test_results:
-            expected_result = expected_results[index]
-            for key, test_value in test_result.items():
-                expected_value = expected_result[key]
-                self.assertEqual(expected_value, test_value,
-                                 msg='Expected {} returned {}'.format(expected_value,
-                                                                      test_value))
+                             'spans':[[2, 8]]}]
+        check_results(expected_results, dong(test_file_path))
 
-        # Ensure the Sentiment is on a 1, 0, -1 scale in the train and test data
-        # and it can be read from the config file
         bad_sent_path = read_config('unit_test_dong_bad_sent_data')
         with self.assertRaises(ValueError, msg='It should not accept sentiment '\
                                'values that are not 1, 0, or -1'):
             dong(bad_sent_path)
 
         # Ensure that it can handle the same target with multiple spans
-        test_multiple_path = read_config('unit_test_dong_bad_multiple_data')
+        test_multiple_path = read_config('unit_test_dong_multiple_offsets_data')
+
+        multi_expected = [{'id':0,
+                           'sentiment':-1,
+                           'text':'This is a fake news article that is to represent a '\
+                           'Tweet!!!! and it was an awful news article I think.',
+                           'target':'news article',
+                           'spans':[[15, 27], [81, 93]]},
+                          {'id':1,
+                           'sentiment':1,
+                           'text':'I had a great day however I did not get much '\
+                           'work done in the day',
+                           'target':'day',
+                           'spans':[[14, 17], [62, 65]]}]
+        check_results(multi_expected, dong(test_multiple_path))
