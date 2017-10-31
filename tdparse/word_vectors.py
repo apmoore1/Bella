94c46f5a89c602efd2c00dedab7dd4807724f973
Andrew Moore <a.moore@lancaster.ac.uk>
2017-10-31 18:09:31 +0000

    Refactored word vectors and added pre-trained

--- tdparse/word_vectors.py
+++ tdparse/word_vectors.py
@@ -1,7 +1,6 @@
 '''
 Contains classes that train and/or load semantic vectors.
 
-
 Classes:
 1. GensimVectors - Creates `Word2Vec <https://arxiv.org/pdf/1301.3781.pdf>`_
 and `FastText <https://arxiv.org/abs/1607.04606>`_ vectors.
@@ -15,67 +14,23 @@ import numpy as np
 from gensim.models import word2vec
 from gensim.models.wrappers import FastText
 
-class GensimVectors(object):
-    '''
-    Class that can create one of the following word vector models:
+class WordVectors(object):
 
-    1. `Word2Vec <https://radimrehurek.com/gensim/models/word2vec.html>`_.
-    2. `Fasttext <https://radimrehurek.com/gensim/models/wrappers/fasttext.html>`_
-
-    instance attributes:
-    1. self.model = Instance of the chosen model
-    2. self.vector_size = Size of the word vectors computed e.g. 100
-    '''
-
-    def __init__(self, file_path, train_data, model=None, **kwargs):
-        '''
-        Trains or loads the model specified.
-
-        :param file_path: Path to the saved model or Path to save the model
-        once trained. Can be None if you only want to train.
-        :param train_data: An object like a list that can be iterated that contains
-        tokenised text, to train the model. Not required if `file_path` contains
-        a trained model.
-        :param model: The name of the model
-        :type file_path: String
-        :type train_data: iterable object e.g. list
-        :type model: String
-        '''
-
-        allowed_models = {'word2vec' : word2vec.Word2Vec,
-                          'fasttext' : FastText}
-        if model not in allowed_models:
-            raise ValueError('model parameter has to be one of the following {} '\
-                             'not {}'.format(allowed_models.keys(), model))
-        model = allowed_models[model]
-        failed_to_load = True
-
-        if isinstance(file_path, str):
-            file_path = os.path.abspath(file_path)
-
-        if isinstance(file_path, str):
-            if os.path.isfile(file_path):
-                try:
-                    self.model = model.load(file_path)
-                    failed_to_load = False
-                except EOFError:
-                    failed_to_load = True
-
-        if hasattr(train_data, '__iter__') and failed_to_load:
-            # Generators throws an error in Gensim
-            if isinstance(train_data, types.GeneratorType):
-                train_data = map(lambda x: x, train_data)
-            self.model = model(train_data, **kwargs)
-            if isinstance(file_path, str):
-                self.model.save(file_path)
-                print('{} model has been saved to {}'.format(model.__name__,
-                                                             file_path))
-        elif failed_to_load:
-            raise Exception('Cannot create model as there is no path to extract '\
-                            'a model from {} or any data to train on which has '\
-                            'to have the __iter__ function {}'\
-                            .format(file_path, train_data))
-        self.vector_size = self.model.wv[self.index2word[0]].shape[0]
+    def __init__(self, word2vector, word_list):
+        if not isinstance(word_list, list):
+            raise TypeError('word_list should be of type list not {}'\
+                            .format(type(word_list)))
+        try:
+            word2vector[word_list[0]]
+            if isinstance(word2vector, list):
+                raise Exception('word2vector cannot be a list')
+        except Exception as exception:
+            raise TypeError('Exception {}: word2vector parameter should be a dict'\
+                  ' type Structure that has word list values as keys'\
+                  .format(exception))
+        self._word2vector = word2vector
+        self._word_list = word_list
+        self.vector_size = word2vector[word_list[0]].shape[0]
 
     def lookup_vector(self, word):
         '''
@@ -91,7 +46,7 @@ class GensimVectors(object):
 
         if isinstance(word, str):
             try:
-                return self.model.wv[word]
+                return self._word2vector[word]
             except KeyError:
                 return np.zeros(self.vector_size)
         raise ValueError('The word parameter must be of type str not {}'\
@@ -106,7 +61,7 @@ class GensimVectors(object):
         '''
 
         index_word = {}
-        for index, word in enumerate(self.model.wv.index2word):
+        for index, word in enumerate(self._word_list):
             index_word[index] = word
         return index_word
 
@@ -133,3 +88,102 @@ class GensimVectors(object):
         for index, word in self.index2word.items():
             index_vector[index] = self.lookup_vector(word)
         return index_vector
+
+
+class GensimVectors(WordVectors):
+    '''
+    Class that can create one of the following word vector models:
+
+    1. `Word2Vec <https://radimrehurek.com/gensim/models/word2vec.html>`_.
+    2. `Fasttext <https://radimrehurek.com/gensim/models/wrappers/fasttext.html>`_
+
+    private attributes:
+    1. self._model = Gensim instance of the chosen model e.g. if word2vec
+    was chosen then it would be `gensim.models.word2vec.Word2Vec`
+    '''
+
+    def __init__(self, file_path, train_data, model=None, **kwargs):
+        '''
+        Trains or loads the model specified.
+
+        :param file_path: Path to the saved model or Path to save the model
+        once trained. Can be None if you only want to train.
+        :param train_data: An object like a list that can be iterated that contains
+        tokenised text, to train the model e.g. [['hello', 'how'], ['another']].
+        Not required if `file_path` contains a trained model.
+        :param model: The name of the model
+        :type file_path: String
+        :type train_data: iterable object e.g. list
+        :type model: String
+        '''
+
+        allowed_models = {'word2vec' : word2vec.Word2Vec,
+                          'fasttext' : FastText}
+        if model not in allowed_models:
+            raise ValueError('model parameter has to be one of the following {} '\
+                             'not {}'.format(allowed_models.keys(), model))
+        model = allowed_models[model]
+        failed_to_load = True
+
+        if isinstance(file_path, str):
+            file_path = os.path.abspath(file_path)
+
+        if isinstance(file_path, str):
+            if os.path.isfile(file_path):
+                try:
+                    self._model = model.load(file_path)
+                    failed_to_load = False
+                except EOFError:
+                    failed_to_load = True
+
+        if hasattr(train_data, '__iter__') and failed_to_load:
+            # Generators throws an error in Gensim
+            if isinstance(train_data, types.GeneratorType):
+                train_data = map(lambda x: x, train_data)
+            self._model = model(train_data, **kwargs)
+            if isinstance(file_path, str):
+                self._model.save(file_path)
+                print('{} model has been saved to {}'.format(model.__name__,
+                                                             file_path))
+        elif failed_to_load:
+            raise Exception('Cannot create model as there is no path to extract '\
+                            'a model from {} or any data to train on which has '\
+                            'to have the __iter__ function {}'\
+                            .format(file_path, train_data))
+        super().__init__(self._model.wv, self._model.wv.index2word)
+
+class PreTrained(WordVectors):
+    '''
+    Class that loads word vectors that have been pre-trained.
+
+    All pre-trained word vectors have to follow the following conditions:
+    1. New word vector on each line
+    2. Each line is tab seperated
+    3. The first tab sperated value on the line is the word
+    4. The rest of the tab seperated values on that line represent the values
+    for the associtaed word.
+    '''
+
+    def __init__(self, file_path):
+        if not isinstance(file_path, str):
+            raise TypeError('The type of the file path should be str not {}'\
+                            .format(type(file_path)))
+        file_path = os.path.abspath(file_path)
+        if not os.path.isfile(file_path):
+            raise ValueError('There is no file at file path {}'.format(file_path))
+
+        word2vector = {}
+        word_list = []
+        with open(file_path, 'r') as data_file:
+            for line in data_file:
+                line = line.strip()
+                word_values = line.split('\t')
+                word = word_values[0]
+                word_list.append(word)
+                word_vector = np.asarray(word_values[1:], dtype='float32')
+                if word in word2vector:
+                    raise KeyError('{} already has a vector in the word vector '\
+                                   'dict'.format(word))
+                else:
+                    word2vector[word] = word_vector
+        super().__init__(word2vector, word_list)
--- tests/test_word_vectors.py
+++ tests/test_word_vectors.py
@@ -9,14 +9,61 @@ import numpy as np
 
 from tdparse.helper import read_config
 import tdparse.tokenisers as tokenisers
+from tdparse.word_vectors import WordVectors
 from tdparse.word_vectors import GensimVectors
+from tdparse.word_vectors import PreTrained
 
 class TestWordVectors(TestCase):
     '''
     Contains the following functions:
 
-    1. test_gensim_word2vec
+    1. test_wordvector_methods
+    2. test_gensim_word2vec
+    3. test_pre_trained
     '''
+
+    def test_wordvector_methods(self):
+        '''
+        Tests the :py:class:`tdparse.word_vectors.WordVectors`
+        '''
+        # Testing the constructor
+        with self.assertRaises(TypeError, msg='Should only accept dicts not lists '\
+                               'for word2vector parameter'):
+            WordVectors([1, 2, 3], [0])
+        with self.assertRaises(TypeError, msg='Should only accept lists'):
+            WordVectors({'something' : np.asarray([5, 6])}, 'something')
+
+        hello_vec = np.asarray([0.5, 0.3, 0.4])
+        another_vec = np.asarray([0.3333, 0.2222, 0.1111])
+        test_vectors = {'hello' : hello_vec,
+                        'another' : another_vec}
+        word_vector = WordVectors(test_vectors, list(test_vectors.keys()))
+
+        vec_size = word_vector.vector_size
+        self.assertEqual(vec_size, 3, msg='Vector size should be 3 not {}'\
+                         .format(vec_size))
+
+        # Testing the methods
+        hello_lookup = word_vector.lookup_vector('hello')
+        self.assertEqual(True, np.array_equal(hello_lookup, hello_vec), msg='{} '\
+                         'should equal {}'.format(hello_lookup, hello_vec))
+
+        zero_vec = np.zeros(3)
+        nothing_vec = word_vector.lookup_vector('nothing')
+        self.assertEqual(True, np.array_equal(zero_vec, nothing_vec), msg='{} '\
+                         'should be a zero vector'.format(nothing_vec))
+
+        index2word = word_vector.index2word
+        word2index = word_vector.word2index
+        index2vector = word_vector.index2vector
+
+        another_index = word2index['another']
+        self.assertEqual('another', index2word[another_index], msg='index2word '\
+                         'and word2index do not match on word `another`')
+        index_correct = np.array_equal(index2vector[another_index], another_vec)
+        self.assertEqual(True, index_correct, msg='index2vector does not return '\
+                         'the correct vector for `another`')
+
 
     def test_gensim_word2vec(self):
         '''
@@ -24,7 +71,7 @@ class TestWordVectors(TestCase):
         '''
 
         # Test loading word vectors from a file
-        vo_zhang_path = os.path.abspath(read_config('word2vec_files')['vo_zhang'])
+        vo_zhang_path = read_config('word2vec_files')['vo_zhang']
         vo_zhang = GensimVectors(vo_zhang_path, None, model='word2vec')
 
         self.assertEqual(vo_zhang.vector_size, 100, msg='Vector size should be equal'\
@@ -93,3 +140,21 @@ class TestWordVectors(TestCase):
                                                  saved_vector.lookup_vector('sherlock'))
                 self.assertEqual(True, equal_sherlocks, msg='The saved model and '\
                                  'the trained model should have the same vectors')
+    def test_pre_trained(self):
+        '''
+        Tests the :py:class:`tdparse.word_vectors.PreTrained`
+        '''
+
+        # Test constructor
+        with self.assertRaises(TypeError, msg='Should not accept a list when '\
+                               'file path is expect to be a String'):
+            PreTrained(['a fake file path'])
+        with self.assertRaises(ValueError, msg='Should not accept strings that '\
+                               'are not file paths'):
+            PreTrained('file.txt')
+        # Test if model loads correctly
+        sswe_path = read_config('sswe_files')['vo_zhang']
+        sswe_model = PreTrained(sswe_path)
+        sswe_vec_size = sswe_model.vector_size
+        self.assertEqual(sswe_vec_size, 50, msg='Vector size should be 50 not '\
+                         '{}'.format(sswe_vec_size))
