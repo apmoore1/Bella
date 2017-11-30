a224fa18b1f896d48ff5062f47a7dffb79f9bade
Andrew Moore <a.moore@lancaster.ac.uk>
2017-11-30 11:50:20 +0000

    Added contexts

---
+++ tdparse/syntactic_contexts.py
@@ -0,0 +1,156 @@
+'''
+A set of functions which either produce contexts and related targets based on
+syntactic parsing. Or functions that create
+'''
+
+def target_normalisation(target_dict):
+    '''
+    Given a target dictionary it normalises the target by removing whitespaces
+    between target words and inserting `_`. Then inserting the normalised word
+    into where the target spans appear and adding whitespace around the target
+    word incase other words are joined on. Returns the text with the inserted
+    normalised words and the normalised target word.
+
+    :param target_dict: Dictionary containing at least `text`, `target` and \
+    `spans` keys with there respected values.
+    :type target_dict: dict
+    :returns: Tuple of two Strings containing. Text with normalised targets and \
+    the normalised target
+    :rtype: tuple
+    '''
+
+    if not isinstance(target_dict, dict):
+        raise TypeError('target_dict parameter has to be of type dict and not '\
+                        '{}'.format(type(target_dict)))
+
+    sorted_spans = sorted(target_dict['spans'], key=lambda span: span[0],
+                          reverse=True)
+    norm_target = '_'.join(target_dict['target'].split())
+    org_text = target_dict['text']
+    for start_index, end_index in sorted_spans:
+        start_text = org_text[: start_index]
+        end_text = org_text[end_index :]
+        start_text += ' ' + norm_target + ' '
+        org_text = start_text + end_text
+    org_text = ' '.join(org_text.split())
+    return org_text, norm_target
+
+def dependency_context(target_dicts, parser, lower=False):
+    '''
+    Given a list of target dicts it will normalise the target word to ensure
+    that it is seperated and if it is a multi word target join the target words
+    together to ensure when it is processed by the dependency parser it is treated
+    as a singular word.
+
+
+    Given a list of target sentences returns a list of contexts where each contexts
+    is associated to a target sentence and each contexts contains a target context
+    for each target word in the target sentence. A target context is a dict which
+    contains `text` and `span` keys where the values correspond to all the
+    dependency related words as a String and the span are the indexs to the target
+    word within the text.
+
+    :param target_dicts: list of dictionaries where each dictionary is associated \
+    to a target sentence.
+    :param parser: function that performs dependency parsing
+    :param lower: Whether to lower case the texts before processing them with the \
+    parser.
+    :type target_dicts: list
+    :type parser: function
+    :type lower: bool Default False
+    :returns: A list of a list of dicts where each list is contains many contexts.
+    :rtype: list
+    '''
+
+    # Normalise the target and text
+    all_text = []
+    all_norm_targets = []
+    for target_dict in target_dicts:
+        norm_text, norm_target = target_normalisation(target_dict)
+        if lower:
+            norm_text = norm_text.lower()
+            norm_target = norm_target.lower()
+        all_text.append(norm_text)
+        all_norm_targets.append(norm_target)
+    # Get contexts
+    all_dependency_tokens = parser(all_text)
+    all_contexts = []
+    for index, dependency_tokens in enumerate(all_dependency_tokens):
+        contexts = []
+        norm_target = all_norm_targets[index]
+        for dependency_token in dependency_tokens:
+            if dependency_token.token.lower() == norm_target.lower():
+                connected_text, target_span = dependency_token.connected_target_span()
+                contexts.append({'text' : connected_text, 'span' : target_span})
+        rel_target = target_dicts[index]
+        valid_num_targets = len(rel_target['spans'])
+        if valid_num_targets != len(contexts):
+            raise ValueError('The number of identified targets `{}` not equal '\
+                             'to the number of targets in the data `{}`'\
+                             .format(contexts, rel_target))
+        # Ensure the returned data type is consistent
+        if contexts == []:
+            raise ValueError('This should not happen as each data type should '\
+                             'have a target {}'.format(rel_target))
+        all_contexts.append(contexts)
+    return all_contexts
+
+def context(all_context_dicts, specific_context, inc_target=False):
+    '''
+    Returns a list of a list of Strings based on the location of the target word
+    in the text within the target dict (NOTE the target word can occur more than
+    once hence why a list is returned as the context is returned for each
+    occurence). Context can be one of the following:
+
+    1. left - left of the target occurence.
+    2. right - right of the target occurence.
+    3. target - target word/words of each target occurence.
+    4. full - whole text repeated for each occurence.
+
+    If the target only occur once in the text then for that text occurence the
+    length of the list will be one.
+
+    :param target_dict: Dictionary that contains text and the spans of the \
+    target word in the text.
+    :param specific_context: String specifying the context e.g. left.
+    :param inc_target: Whether to include the target word in the context text. \
+    (Only applies for left and right context.)
+    :type target_dict: dict
+    :type specific_context: String
+    :type inc_target: Boolean Default False
+    :returns: A list of of a list of context strings
+    :rtype: list
+    '''
+
+    acceptable_contexts = {'left', 'right', 'target', 'full'}
+    if specific_context not in acceptable_contexts:
+        raise ValueError('context parameter can only be one of the following {}'\
+                         ' not {}'.format(acceptable_contexts, context))
+    all_contexts = []
+    for context_dicts in all_context_dicts:
+        contexts = []
+        for context_dict in context_dicts:
+            text = context_dict['text']
+            target_span = context_dict['span']
+            start_char = target_span[0]
+            end_char = target_span[1]
+            if specific_context == 'left':
+                if inc_target:
+                    contexts.append(text[:end_char])
+                else:
+                    contexts.append(text[:start_char])
+            elif specific_context == 'right':
+                if inc_target:
+                    contexts.append(text[start_char:])
+                else:
+                    contexts.append(text[end_char:])
+            elif specific_context == 'target':
+                contexts.append(text[start_char:end_char])
+            elif specific_context == 'full':
+                contexts.append(text)
+            else:
+                raise ValueError('context parameter should only be `right` or '\
+                                 '`left` not {} there must be a logic error'\
+                                 .format(specific_context))
+        all_contexts.append(contexts)
+    return all_contexts
---
+++ tests/test_syntactic_contexts.py
@@ -0,0 +1,217 @@
+'''
+Unit test suite for the :py:mod:`tdparse.syntactic_contexts` module.
+'''
+from unittest import TestCase
+
+from tdparse.syntactic_contexts import context
+from tdparse.syntactic_contexts import target_normalisation
+from tdparse.syntactic_contexts import dependency_context
+from tdparse.dependency_parsers import tweebo
+
+class TestTarget(TestCase):
+    '''
+    Contains the following functions:
+    '''
+
+    def test_target_normlisation(self):
+        '''
+        Tests target_normalisation
+        '''
+
+        with self.assertRaises(TypeError, msg='target_dict parameter has to '\
+                               'be of type dict only'):
+            target_normalisation(['anything'])
+
+        test_values = [{'id':0,
+                        'sentiment':-1,
+                        'text':'This is a fake news articledd that is to represent a '\
+                        'Tweet!!!! and it was an awful ssNews Articless I think.',
+                        'target':'news article',
+                        'spans':[[15, 27], [85, 97]]},
+                       {'id':1,
+                        'sentiment':1,
+                        'text':'I had a great ssDay however I did not get much '\
+                        'work done in the days',
+                        'target':'day',
+                        'spans':[[16, 19], [64, 67]]}]
+        valid_results = [('This is a fake news_article dd that is to represent a '\
+                          'Tweet!!!! and it was an awful ss news_article ss I think.',
+                          'news_article'),
+                         ('I had a great ss day however I did not get much '\
+                          'work done in the day s',
+                          'day')]
+        for index, test_value in enumerate(test_values):
+            test_result = target_normalisation(test_value)
+            valid_result = valid_results[index]
+            self.assertEqual(valid_result, test_result, msg='Results is '\
+                             '{} and should be {}'\
+                             .format(valid_result, test_result))
+    def test_dependency_context(self):
+        '''
+        Tests dependency_context
+        '''
+
+        # Test the normalise case
+        test_values = [{'id':0,
+                        'sentiment':-1,
+                        'text':'This is a fake news articledd that is to represent a '\
+                        'Tweet!!!! and it was an awful ssNews Articless I think.',
+                        'target':'news article',
+                        'spans':[[15, 27], [85, 97]]},
+                       {'id':1,
+                        'sentiment':1,
+                        'text':'I had a great ssDay however I did not get much '\
+                        'work done in the days',
+                        'target':'day',
+                        'spans':[[16, 19], [64, 67]]}]
+        valid_results = [[{'text' : 'This is a fake news_article',
+                           'span' : [15, 27]},
+                          {'text' : 'dd that is to represent a Tweet and it was '\
+                                    'an awful news_article',
+                           'span' : [52, 64]}],
+                         [{'text' : 'I had a great day however I did not get '\
+                                    'much work done in the day',
+                           'span' : [14, 17]},
+                          {'text' : 'I had a great day however I did not get '\
+                                    'much work done in the day',
+                           'span' : [62, 65]}]]
+        test_results = dependency_context(test_values, tweebo)
+        for index, valid_result in enumerate(valid_results):
+            test_result = test_results[index]
+            for dict_index, valid_dict in enumerate(valid_result):
+                test_dict = test_result[dict_index]
+                self.assertEqual(valid_dict['text'], test_dict['text'],
+                                 msg='texts are different correct `{}` test `{}`'\
+                                     .format(valid_dict['text'], test_dict['text']))
+                self.assertEqual(valid_dict['span'], test_dict['span'],
+                                 msg='spans are different correct `{}` test `{}`'\
+                                     .format(valid_dict['span'], test_dict['span']))
+        # Test the lower casing case
+        test_values = [{'id':0,
+                        'sentiment':-1,
+                        'text':'This is a fake news articledd that is to represent a '\
+                        'Tweet!!!! and it was an awful ssNews Articless I think.',
+                        'target':'news article',
+                        'spans':[[15, 27], [85, 97]]},
+                       {'id':1,
+                        'sentiment':1,
+                        'text':'I had a great ssDay however I did not get much '\
+                        'work done in the days',
+                        'target':'day',
+                        'spans':[[16, 19], [64, 67]]}]
+        valid_results = [[{'text' : 'this is a fake news_article',
+                           'span' : [15, 27]},
+                          {'text' : 'dd that is to represent a tweet and it was '\
+                                    'an awful news_article',
+                           'span' : [52, 64]}],
+                         [{'text' : 'i had a great day however i did not get '\
+                                    'much work done in the day',
+                           'span' : [14, 17]},
+                          {'text' : 'i had a great day however i did not get '\
+                                    'much work done in the day',
+                           'span' : [62, 65]}]]
+        test_results = dependency_context(test_values, tweebo, lower=True)
+        for index, valid_result in enumerate(valid_results):
+            test_result = test_results[index]
+            for dict_index, valid_dict in enumerate(valid_result):
+                test_dict = test_result[dict_index]
+                self.assertEqual(valid_dict['text'], test_dict['text'],
+                                 msg='texts are different correct `{}` test `{}`'\
+                                     .format(valid_dict['text'], test_dict['text']))
+                self.assertEqual(valid_dict['span'], test_dict['span'],
+                                 msg='spans are different correct `{}` test `{}`'\
+                                     .format(valid_dict['span'], test_dict['span']))
+    def test_context(self):
+        '''
+        Tests context
+        '''
+
+        def test_contexts(all_valid_results, all_test_results):
+            '''
+            :param all_valid_results: A list of a list of Strings that are the \
+            correct values.
+            :param all_test_results: A list of a list of Strings that are the \
+            values being tested.
+            :type all_valid_results: list
+            :type all_test_results: list
+            :returns: Nothing. It tests if the Strings found it the test results \
+            are the same as those in the valid results else raises an assertion \
+            error.
+            :rtype: None
+            '''
+
+            for index, valid_results in enumerate(all_valid_results):
+                for inner_index, valid_result in enumerate(valid_results):
+                    test_result = all_test_results[index][inner_index]
+                    self.assertEqual(valid_result, test_result, msg='context '\
+                                     'should be {} and not {}'\
+                                     .format(valid_result, test_result))
+
+        test_values = [[{'text' : 'This is a fake news_article',
+                         'span' : [15, 27]},
+                        {'text' : 'dd that is to represent a Tweet and it was '\
+                                  'an awful news_article',
+                         'span' : [52, 64]}],
+                       [{'text' : 'I had a great day however I did not get '\
+                                  'much work done in the day',
+                         'span' : [14, 17]},
+                        {'text' : 'I had a great day however I did not get '\
+                                  'much work done in the day',
+                         'span' : [62, 65]}]]
+        # Testing all four contexts: 1. left, 2. right, 3. target and 4. full
+        valid_left_results = [['This is a fake ',
+                               'dd that is to represent a Tweet and it was an '\
+                               'awful '],
+                              ['I had a great ', 'I had a great day however I '\
+                               'did not get much work done in the ']]
+        test_left_results = context(test_values, 'left')
+        test_contexts(valid_left_results, test_left_results)
+
+        valid_right_results = [['', ''],
+                               [' however I did not get much work done in the day',
+                                '']]
+        test_right_results = context(test_values, 'right')
+        test_contexts(valid_right_results, test_right_results)
+
+        valid_target_results = [['news_article', 'news_article'], ['day', 'day']]
+        test_target_results = context(test_values, 'target')
+        test_contexts(valid_target_results, test_target_results)
+
+        valid_full_results = [['This is a fake news_article',
+                               'dd that is to represent a Tweet and it was '\
+                               'an awful news_article'],
+                              ['I had a great day however I did not get much '\
+                               'work done in the day',
+                               'I had a great day however I did not get much '\
+                                'work done in the day']]
+        test_full_results = context(test_values, 'full')
+        test_contexts(valid_full_results, test_full_results)
+
+        # Testing all contexts but including the target
+        valid_left_results = [['This is a fake news_article',
+                               'dd that is to represent a Tweet and it was an '\
+                               'awful news_article'],
+                              ['I had a great day', 'I had a great day however I '\
+                               'did not get much work done in the day']]
+        test_left_results = context(test_values, 'left', inc_target=True)
+        test_contexts(valid_left_results, test_left_results)
+
+        valid_right_results = [['news_article', 'news_article'],
+                               ['day however I did not get much work '\
+                                'done in the day', 'day']]
+        test_right_results = context(test_values, 'right', inc_target=True)
+        test_contexts(valid_right_results, test_right_results)
+
+        valid_target_results = [['news_article', 'news_article'], ['day', 'day']]
+        test_target_results = context(test_values, 'target', inc_target=True)
+        test_contexts(valid_target_results, test_target_results)
+
+        valid_full_results = [['This is a fake news_article',
+                               'dd that is to represent a Tweet and it was '\
+                               'an awful news_article'],
+                              ['I had a great day however I did not get much '\
+                               'work done in the day',
+                               'I had a great day however I did not get much '\
+                                'work done in the day']]
+        test_full_results = context(test_values, 'full', inc_target=True)
+        test_contexts(valid_full_results, test_full_results)
