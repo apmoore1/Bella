981bddea7ab948176d866f8019e8af4d3a5c24ce
Andrew Moore <andrew.p.moore94@gmail.com>
2018-04-19 17:01:21 +0100

    Updated the package struct

---
+++ tdparse/scikit_features/__init__.py
@@ -0,0 +1,3 @@
+from tdparse.scikit_features import context, debug, join_context_vectors
+from tdparse.scikit_features import lexicon_filter, neural_pooling
+from tdparse.scikit_features import syntactic_context, tokeniser, word_vector
