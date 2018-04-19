ec6feef08a434b5f9723ef8b437027ffd7e9d30a
Andrew Moore <andrew.p.moore94@gmail.com>
2018-04-19 17:03:23 +0100

    Stanford now singleton

--- tdparse/stanford_tools.py
+++ tdparse/stanford_tools.py
@@ -3,17 +3,47 @@ import json
 from nltk.tree import Tree
 from stanfordcorenlp import StanfordCoreNLP
 
-STANFORD_NLP = StanfordCoreNLP('http://localhost', port=9000)
+
+class StanfordNlp(object):
+    '''
+    Singleton Class instance
+    '''
+
+    instance = None
+
+    def __new__(cls) -> StanfordCoreNLP:
+        if StanfordNlp.instance is None:
+            StanfordNlp.instance = StanfordCoreNLP
+        return StanfordNlp.instance
+
+    def __init__(self, ip_address='http://localhost', port=9000) -> None:
+        '''
+        :param ip_address: Address of the Stanford Core NLP server
+        :param port: port that the Stanford Core NLP server is listening to
+        :type ip_address: str
+        :type port: int
+        '''
+
+        self.instance = self.instance(ip_address, port)
+
+    def __getattr__(self, name):
+        return getattr(self.instance, name)
+
+    def __setattr__(self, name, value):
+        return setattr(self.instance, name, value)
 
 
 def tokenise(text):
-    output_dict = STANFORD_NLP.annotate(text, {'annotators' : 'ssplit,tokenize',
-                                               'tokenize.language' : 'English',
-                                               'outputFormat' : 'json'})
+
+    stanford_nlp = StanfordNlp()
+    output_dict = stanford_nlp.annotate(text, {'annotators': 'ssplit,tokenize',
+                                               'tokenize.language': 'English',
+                                               'outputFormat': 'json'})
     output_dict = json.loads(output_dict)
-    tokens = [token['originalText'] for s in output_dict['sentences'] \
+    tokens = [token['originalText'] for s in output_dict['sentences']
               for token in s['tokens']]
     return tokens
+
 
 def constituency_parse(text):
     '''
@@ -24,38 +54,44 @@ def constituency_parse(text):
     If one sentence in the text then the list will be of length 1.
     :rtype: list
     '''
+
     if text.strip() == '':
-        raise ValueError('There has to be some text to parse. Text given {}'\
+        raise ValueError('There has to be some text to parse. Text given {}'
                          .format(text))
-    output_dict = STANFORD_NLP.annotate(text, {'annotators' : 'pos,parse',
-                                               'tokenize.language' : 'English',
-                                               'outputFormat' : 'json'})
+    stanford_nlp = StanfordNlp()
+    output_dict = stanford_nlp.annotate(text, {'annotators': 'pos,parse',
+                                               'tokenize.language': 'English',
+                                               'outputFormat': 'json'})
     output_dict = json.loads(output_dict)
-    parse_trees = [Tree.fromstring(sent['parse']) \
+    parse_trees = [Tree.fromstring(sent['parse'])
                    for sent in output_dict['sentences']]
     return parse_trees
+
 
 def dependency_parse(text, dep_type='basicDependencies'):
     '''
     :param text: The text you want to parse
     :param dep_type: The dependency type to use either: 'basicDependencies', \
-    'enhancedDependencies', or 'enhancedPlusPlusDependencies'. See for more details \
+    'enhancedDependencies', or 'enhancedPlusPlusDependencies'. See for more \
+    details \
     https://nlp.stanford.edu/~sebschu/pubs/schuster-manning-lrec2016.pdf
     :type text: String
     :type dep_type: String. Default basicDependencies
     '''
 
     if text.strip() == '':
-        raise ValueError('There has to be some text to parse. Text given {}'\
+        raise ValueError('There has to be some text to parse. Text given {}'
                          .format(text))
-    output_dict = STANFORD_NLP.annotate(text, {'annotators' : 'pos,depparse',
-                                               'tokenize.language' : 'English',
-                                               'outputFormat' : 'json'})
+
+    stanford_nlp = StanfordNlp()
+    output_dict = stanford_nlp.annotate(text, {'annotators': 'pos,depparse',
+                                               'tokenize.language': 'English',
+                                               'outputFormat': 'json'})
     sentences = json.loads(output_dict)['sentences']
     tokens_dicts = []
     dep_dicts = []
     for sentence in sentences:
-        tokens_dict = {token_data['index'] : token_data \
+        tokens_dict = {token_data['index']: token_data
                        for token_data in sentence['tokens']}
         dep_dict = {}
         for dep_data in sentence[dep_type]:
