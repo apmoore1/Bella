41d129c2e75905a2e6d910a2f0133b1c94523a88
Andrew Moore <andrew.p.moore94@gmail.com>
2019-02-05 17:35:49 +0000

    Update twine problems

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r") as fh:
 
 
 setup(name='bella_tdsa',
-      version='0.3.23',
+      version='0.3.3',
       description='Target Dependent Sentiment Analysis (TDSA) framework.',
       long_description=long_description,
       long_description_content_type="text/markdown",
